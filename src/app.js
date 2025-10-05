f16f651217643c4ab1303e1284eb0884c5861b31
RenzoVargas <renzo.9217@gmail.com>
2025-10-05 15:46:09 +0200

    finish project

--- src/app.js
+++ src/app.js
@@ -6,22 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("He el consolelog")
+  // console.log("Hace el consolelog")
+  generateExcuse();
+}
+function generateExcuse() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class','when I was sleeping','while I was exercising','during my lunch','while I was praying'];
 
+  let whoPart = who[Math.floor(Math.random() * who.length)];
+  let actionPart = action[Math.floor(Math.random() * action.length)];
+  let whatPart = what[Math.floor(Math.random() * what.length)];
+  let whenPart = when[Math.floor(Math.random() * when.length)];
 
+  let excuse = `${whoPart} ${actionPart} ${whatPart} ${whenPart}.`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-};
+  document.getElementById("excuse").innerHTML = excuse; }
