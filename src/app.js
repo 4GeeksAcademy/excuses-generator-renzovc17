661dc7ab3a7be688789f98b098c91e2cd5fc49aa
RenzoVargas <renzo.9217@gmail.com>
2025-10-05 13:01:11 +0200

    firstcommit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("He el consolelog")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
